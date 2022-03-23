085388ee5a3d03b81b511f2184d89cc82744f05f
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2022-03-23 12:00:59 +0000

    Apollo: Init EvolutionX

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The ArrowOS Project
+# Copyright (C) 2021 The evolutionOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_apollo.mk
+    $(LOCAL_DIR)/evolution_apollo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_apollo-user \
-    arrow_apollo-userdebug \
-    arrow_apollo-eng
+    evolution_apollo-user \
+    evolution_apollo-userdebug \
+    evolution_apollo-eng
--- evolution_apollo.mk
+++ evolution_apollo.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The ArrowOS Project
+# Copyright (C) 2021 The evolutionOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,17 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
-$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
+# Inherit some common evolution stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 TARGET_GAPPS_ARCH := arm64
 IS_PHONE := true
-DEVICE_MAINTAINER := Dobsgw
+
+# Inherit some other EVOLUTION stuff.
+WITH_GAPPS := true
+EVO_BUILD_TYPE := OFFICIAL
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+BUILD_USERNAME := AGMAD
+BUILD_HOSTNAME := Evolution-X
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
-PRODUCT_NAME := arrow_apollo
+PRODUCT_NAME := evolution_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -32,4 +41,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="apollo_global-user 11 RKQ1.200826.002 V12.5.5.0.RJDMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/apollo_global/apollo:11/RKQ1.200826.002/V12.5.5.0.RJDMIXM:user/release-keys
